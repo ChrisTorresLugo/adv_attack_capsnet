d22f45ac87c4e7890ae277f4e872331213eab9c6
christorreslugo <christorreslugo@gmail.com>
2018-04-21 17:13:11 -0400

    Changed default iter value to 1500 to compare with git results

--- code/attack_llcm.py
+++ code/attack_llcm.py
@@ -424,14 +424,10 @@ def main(_):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default=cfg.DATA_DIR,
-                        help='Directory for storing input data')
-    parser.add_argument('--max_epsilon', type=int, default=10,
-                        help='max_epsilon')
-    parser.add_argument('--max_iter', type=int, default=1,
-                        help='max iteration')
-    parser.add_argument('--dataset', type = str, default = "mnist",
-                        help='Dataset used to train the model')
+    parser.add_argument('--data_dir', type=str, default=cfg.DATA_DIR, help='Directory for storing input data')
+    parser.add_argument('--max_epsilon', type=int, default=10, help='max_epsilon')
+    parser.add_argument('--max_iter', type=int, default=1, help='max iteration')
+    parser.add_argument('--dataset', type = str, default = "mnist", help='Dataset used to train the model')
 
     FLAGS, unparsed = parser.parse_known_args()
     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
